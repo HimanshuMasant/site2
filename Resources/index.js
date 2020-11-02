b973022337836dcde3c59b2e882eaa531989847e
HimanshuMasant <72501638+HimanshuMasant@users.noreply.github.com>
2020-11-02 22:32:21 +0530

    Update index.js

--- Resources/index.js
+++ Resources/index.js
@@ -1,5 +1,5 @@
-var num1 = Math.floor((Math.random() * 6));
-var num2 = Math.floor((Math.random() * 6));
+var num1 = Math.floor(Math.random() * 6) + 1;
+var num2 = Math.floor(Math.random() * 6) + 1;
 
 // Dice of Player 1
 
@@ -13,7 +13,7 @@ if (num1 === 1) {
   document.getElementById("pict1").src = "Resources/dice4.png";
 } else if (num1 === 5) {
   document.getElementById("pict1").src = "Resources/dice5.png";
-} else {
+} else if (num1 === 6) {
   document.getElementById("pict1").src = "Resources/dice6.png";
 }
 
@@ -29,16 +29,16 @@ if (num2 === 1) {
   document.getElementById("pict2").src = "Resources/dice4.png";
 } else if (num2 === 5) {
   document.getElementById("pict2").src = "Resources/dice5.png";
-} else {
+} else if (num2 === 6) {
   document.getElementById("pict2").src = "Resources/dice6.png";
 }
 
 // Decleration of Winner
 
 if (num1 > num2) {
-  document.getElementById("winner").textContent = "Player 1 is the winner";
+  document.getElementById("winner").textContent = " Player 1 is the winner ";
 } else if (num1 < num2) {
-  document.getElementById("winner").textContent = "Player 2 is the winner";
-} else {
-  document.getElementById("winner").textContent = " ";
+  document.getElementById("winner").textContent = " Player 2 is the winner ";
+} else if (num1 === num2) {
+  document.getElementById("winner").innerHTML = " ! <em> No one is the winner </em> ! ";
 }
